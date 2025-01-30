ee64a9cd3d629fe5beded53bbc0dfbf89707d6aa
DanyaSuprun <dan.supreme2006@gmail.com>
2025-01-30 17:56:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+double num1, num2, result;
+string operation;
+
+Console.WriteLine("Введіть перше число:");
+num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введіть оператора ");
+operation = Console.ReadLine();
+
+Console.WriteLine("Введіть друге число:");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+switch (operation)
+{
+    case "+":
+        result = num1 + num2;
+        Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+        break;
+    case "-":
+        result = num1 - num2;
+        Console.WriteLine($"Результат: {num1} - {num2} = {result}");
+        break;
+    case "*":
+        result = num1 * num2;
+        Console.WriteLine($"Результат: {num1} * {num2} = {result}");
+        break;
+    case "/":
+        break;
+
+}
