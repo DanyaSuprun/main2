cba6c07d12d368202fccbf68c9b170310a00f5c2
DanyaSuprun <dan.supreme2006@gmail.com>
2025-02-02 16:25:31 +0100

    Add files via upload

---
+++ 3_Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+int num1, num2, result;
+string operation;
+
+Console.WriteLine("Введіть перше число:");
+num1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введіть дію ділення ");
+operation = Console.ReadLine();
+
+Console.WriteLine("Введіть друге число:");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+try
+{
+    Console.WriteLine(num1 / num2);
+}
+
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Ділення на нуль");
+
+ }
+    switch (operation)
+    {
+
+        case "/":
+            result = num1 / num2;
+            Console.WriteLine($"Результат: {num1} / {num2} = {result}");
+            break;
+    }
+  
+
+
+
+
