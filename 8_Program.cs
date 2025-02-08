13e45ca356d004200ef0eea5167c75d8ce20f5e0
DanyaSuprun <dan.supreme2006@gmail.com>
2025-02-08 13:08:58 +0100

    Add files via upload

---
+++ 8_Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+
+    
+        // Запитуємо число у користувача
+        Console.Write("Введіть число для генерації таблиці множення: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Генерація таблиці множення для введеного числа
+        Console.WriteLine($"Таблиця множення для числа {number}:");
+
+        // Цикл для множення числа на числа від 1 до 10
+        for (int i = 1; i <= 15; i++)
+        {
+            int result = number * i;
+            Console.WriteLine($"{number} * {i} = {result}");
+        }
+    
