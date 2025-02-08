2192a5a098e86c5529c420ba0690fbe137fab0d8
DanyaSuprun <dan.supreme2006@gmail.com>
2025-02-08 13:03:53 +0100

    Add files via upload
    Калькулятор оплати праці за годину

---
+++ 4_Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+int num1, num2, result;
+string operation;
+
+Console.WriteLine("кількість годин?:");
+num1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Розмір годинної ставки?:");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+result =  num1 * num2;
+Console.WriteLine("оплата за день:"+ result );
+
+
