e6c472415a252e21d76f7f9b1dc0d72bf2c92eec
DanyaSuprun <dan.supreme2006@gmail.com>
2025-03-17 09:34:36 +0100

    Add files via upload

---
+++ Program231.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Введіть шлях до вихідного файлу: ");
+        string inputFilePath = Console.ReadLine();
+
+        Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+        string outputFilePath = Console.ReadLine();
+
+        try
+        {
+            
+            string content = File.ReadAllText(inputFilePath);
+
+            
+            File.WriteAllText(outputFilePath, content);
+
+            
+            Console.WriteLine("Файл успішно скопійовано!");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Помилка: вихідний файл не знайдено.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Помилка при роботі з файлами: {ex.Message}");
+        }
+    }
+}
