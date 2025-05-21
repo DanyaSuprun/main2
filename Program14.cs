668c9d89db0a45d7ecf4fc264d66808b64c2633f
DanyaSuprun <dan.supreme2006@gmail.com>
2025-05-21 11:49:21 +0200

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MyArrayApp
+{
+    
+    public interface IOutput
+    {
+        void Show();                 
+        void Show(string info);     
+    }
+
+    
+    public class MyArray : IOutput
+    {
+        private int[] array;
+
+        
+        public MyArray(int[] array)
+        {
+            this.array = array;
+        }
+
+        
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 7, 2, 9, 5 };
+
+            MyArray myArray = new MyArray(numbers);
+
+            
+            myArray.Show();
+
+            
+            myArray.Show("Це мій масив чисел:");
+        }
+    }
+}
+
+
+
+
+
